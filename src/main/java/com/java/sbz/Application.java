a48f9e0fa743317414a8e0dd38432cb951063aeb
sirko <diskozub@hotmail.com>
2017-09-01 20:20:51 +0200

    initial commit

---
+++ src/main/java/com/java/sbz/Application.java
@@ -0,0 +1,21 @@
+package com.java.sbz;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
+	}
+
+}
